e14b81675e77e4f0529a1a0d16787b54d5074273
MajorX234 <majorx234@googlemail.com>
2024-11-02 21:00:16 +0100

    Add example parsing a json string

--- src/tree_sitter_json_test.cpp
+++ src/tree_sitter_json_test.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <stdio.h>
 
 extern "C" {
@@ -6,7 +7,17 @@ extern "C" {
 }
 
 int main(void){
-  TSLanguage *p =tree_sitter_json();
+  const char *source_code = "{\"example\":[1, null]}";
+
+  TSLanguage *p = tree_sitter_json();
+  TSParser * parser = ts_parser_new();
+  ts_parser_set_language(parser, tree_sitter_json());
+  TSTree * tree = ts_parser_parse_string(
+    parser,
+    NULL,
+    source_code,
+    strlen(source_code)
+  );
   printf("Hello, Treesitter\n");
   return 0;
 }
