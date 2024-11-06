46ce902c5105b95fdd316950e96129fc77f7da00
MajorX234 <majorx234@googlemail.com>
2024-11-06 01:48:16 +0100

    Add traversing through syntax tree

--- src/tree_sitter_json_test.cpp
+++ src/tree_sitter_json_test.cpp
@@ -7,9 +7,24 @@ extern "C" {
   TSLanguage *tree_sitter_json();
 }
 
-int main(void){
-  const char *source_code = "{\"example\":{\"array1\":[1, null],\"array2\":[2, null],\"some_property\":{\"value1\":12,\"value2\":13}}}";
+void visit_tree (TSNode node) {
+  // Print the syntax tree as an S-expression.
+  char *string = ts_node_string(node);
+  printf("Syntax tree: %s\n", string);
+  free(string);
+  unsigned child_count = ts_node_child_count(node);
 
+  for (unsigned i = 0; i < child_count; i++) {
+    TSNode child_node = ts_node_child(node, i);
+
+    visit_tree(child_node);
+  }
+}
+
+int main(void){
+  //const char *source_code = "{\"example\":{\"array1\":[1, null],\"array2\":[2, null],\"some_property\":{\"value1\":12,\"value2\":13}}}";
+
+  const char *source_code = "{\"example\":[1, null]}";
   TSLanguage *p = tree_sitter_json();
   TSParser * parser = ts_parser_new();
   ts_parser_set_language(parser, tree_sitter_json());
@@ -23,30 +38,7 @@ int main(void){
     // Get the root node of the syntax tree.
   TSNode root_node = ts_tree_root_node(tree);
 
-    // Print the syntax tree as an S-expression.
-  char *string = ts_node_string(root_node);
-  printf("Syntax tree: %s\n", string);
-
-  uint32_t nchildren_root = ts_node_child_count(root_node);
-
-  for (uint32_t i = 0;i<nchildren_root;i++){
-    TSNode child_node = ts_node_child(root_node,i);
-    char *child_string = ts_node_string(root_node);
-    printf("  ");
-    printf("%s\n", child_string);
-    uint32_t nchildren_child = ts_node_child_count(child_node);
-    printf("has %d children\n",nchildren_child);
-    for (uint32_t j = 0;j<nchildren_child;j++){
-      TSNode childchild_node = ts_node_child(child_node,i);
-      char *childchild_string = ts_node_string(root_node);
-      printf("    ");
-      printf("%s\n", childchild_string);
-      free(childchild_string);
-    }
-    free(child_string);
-  }
-  // Free all of the heap-allocated memory.
-  free(string);
+  visit_tree(root_node);
   ts_tree_delete(tree);
   ts_parser_delete(parser);
 
