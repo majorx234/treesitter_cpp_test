ce90b693fd6ce0c851259893f40f70d23bda69d9
MajorX234 <majorx234@googlemail.com>
2024-11-06 02:40:38 +0100

    Add iterative traversal through syntaxtree

--- src/tree_sitter_json_test.cpp
+++ src/tree_sitter_json_test.cpp
@@ -1,13 +1,15 @@
 #include <cstdint>
+#include <cstdlib>
 #include <cstring>
 #include <stdio.h>
 
 extern "C" {
   #include <tree_sitter/api.h>
+  #include "node_stack.h"
   TSLanguage *tree_sitter_json();
 }
 
-void visit_tree (TSNode node) {
+void visit_tree_recursive (TSNode node) {
   // Print the syntax tree as an S-expression.
   char *string = ts_node_string(node);
   printf("Syntax tree: %s\n", string);
@@ -17,7 +19,54 @@ void visit_tree (TSNode node) {
   for (unsigned i = 0; i < child_count; i++) {
     TSNode child_node = ts_node_child(node, i);
 
-    visit_tree(child_node);
+    visit_tree_recursive(child_node);
+  }
+}
+
+void visit_tree_iterative(TSTree * tree){
+  TSNode* node = (TSNode*)malloc(sizeof(TSNode));
+  *node = ts_tree_root_node(tree);
+  NodeStack node_stack = {
+    .arr = {0},
+    .top = 0
+  };
+  NodeStack to_delete = {
+    .arr = {0},
+    .top = 0
+  };;
+
+  push(&node_stack, node);
+
+  printf("startloop\n");
+  while(!is_empty(&node_stack)){
+    TSNode* node = pop(&node_stack);
+    if(node == NULL){
+      printf("error: node_stack empty\n");
+      break;
+    }
+    char *string = ts_node_string(*node);
+    printf("Syntax tree1: %s\n", string);
+    free(string);
+
+    unsigned child_count = ts_node_child_count(*node);
+    for (unsigned i = 0; i < child_count; i++) {
+        TSNode* child_node = (TSNode*)malloc(sizeof(TSNode));
+        *child_node =  ts_node_child(*node, i);
+        push(&node_stack, child_node);
+    }
+    push(&to_delete, node);
+  }
+
+  printf("clean up \n");
+
+  // clean up
+  while(!is_empty(&to_delete)){
+    TSNode* delete_node = pop(&to_delete);
+    if(delete_node == NULL){
+      printf("error: delete node_stack empty\n");
+      break;
+    }
+    free(delete_node);
   }
 }
 
@@ -35,10 +84,13 @@ int main(void){
     strlen(source_code)
   );
 
-    // Get the root node of the syntax tree.
+  // Get the root node of the syntax tree.
+  visit_tree_iterative(tree);
+  printf("finished iterative traversal\n");
   TSNode root_node = ts_tree_root_node(tree);
 
-  visit_tree(root_node);
+  visit_tree_recursive(root_node);
+  printf("finished recursive traversal\n");
   ts_tree_delete(tree);
   ts_parser_delete(parser);
 
